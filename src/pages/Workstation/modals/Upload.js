6cfd1074922868e3890fc7bc3610ea556a02cd0a
rahul21p <patel.r03@gmail.com>
2020-04-29 14:07:08 -0400

    created the two file upload modals

--- src/pages/Workstation/modals/Upload.js
+++ src/pages/Workstation/modals/Upload.js
@@ -3,8 +3,14 @@ import React from "react";
 const Upload = (props) => {
   const { toggleModal } = props;
   return (
-    <div>
-      <h1>UPLOAD PAGE</h1>
+    <div className="upload">
+      <h1 className="upload__Title">Upload your creation</h1>
+      <h2 className="challenge__Description">Select the type of projet you want to upload</h2>
+      <div className="upload__Option">
+        <button className="upload__Option--btn">Audio</button>
+        <button className="upload__Option--btn">Photo</button>
+        <button className="upload__Option--btn">Writing</button>
+      </div>
       <button onClick={() => toggleModal("uploadCreation")}>CLOSE</button>
     </div>
   );
--- src/pages/Workstation/modals/UploadCreation.js
+++ src/pages/Workstation/modals/UploadCreation.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {file} from '../../../assets/fileUpload.png';
+
 
 const UploadCreation = (props) => {
   const { toggleModal } = props;
   return (
-    <div>
-      <h1>UPLOAD creation PAGE</h1>
-      <button onClick={() => toggleModal(false)}>CLOSE</button>
+    <div className="upload">
+      <h1 className="upload__Title">UPLOAD creation PAGE</h1>
+      <div>
+        <img className="upload__File--image" src={file}></img>
+        <input className="upload__File--description" type="Text"></input>
+      </div>
+      <button classname="upload__Bttn" onClick={() => toggleModal(false)}>Upload</button>
     </div>
   );
 };
