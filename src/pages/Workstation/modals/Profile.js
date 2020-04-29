4420e3e90326d73784b9016ef0dc0f151889823f
rahul21p <patel.r03@gmail.com>
2020-04-29 15:01:10 -0400

    finished expanding profile modal

--- src/pages/Workstation/modals/Profile.js
+++ src/pages/Workstation/modals/Profile.js
@@ -3,9 +3,22 @@ import React from "react";
 const Profile = (props) => {
   const { toggleModal } = props;
   return (
-    <div>
-      <h1>Profile PAGE</h1>
-      <button onClick={() => toggleModal(false)}>CLOSE</button>
+    <div className="profile">
+      <h1 className="profile__Title">Anthony Jackson</h1>
+      <div>
+        <img src="" ></img>
+        <div className="profile__Description">
+          <p>Hi I’m Anthony most people call me Andy. I Enjoy cooking, listening to music, and playing with my dog. Looking forward to connecting to other creatives. </p>
+          <p>Location: Vancouver, B.C.</p>
+        </div>
+      </div>
+      <div>
+        <h2 className="profile__Title">Recent Projects</h2>
+        <p className="profile_Description">Vegan Mac and Cheese</p>
+        <p className="profile_Description">Landscape Mountains</p>
+        <p className="profile_Description">Lyrical Poem</p>
+      </div>
+      <button className="profile__Btn" onClick={() => toggleModal(false)}>CLOSE</button>
     </div>
   );
 };
