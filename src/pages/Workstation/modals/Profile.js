ff0fd90d41456a2bf3e68e2df53434c751abc7fc
kayla9394 <noronha.kayla@gmail.com>
2020-04-29 15:27:00 -0400

    profile styles

--- src/pages/Workstation/modals/Profile.js
+++ src/pages/Workstation/modals/Profile.js
@@ -1,24 +1,34 @@
 import React from "react";
-
+import anthony from "../../../assets/anthony.png";
 const Profile = (props) => {
   const { toggleModal } = props;
   return (
     <div className="profile">
       <h1 className="profile__Title">Anthony Jackson</h1>
-      <div>
-        <img src="" ></img>
+      <div className="profile__Section">
+        <img src={anthony} width="96px" height="96px" />
         <div className="profile__Description">
-          <p>Hi I’m Anthony most people call me Andy. I Enjoy cooking, listening to music, and playing with my dog. Looking forward to connecting to other creatives. </p>
-          <p>Location: Vancouver, B.C.</p>
+          <p>
+            Hi I’m Anthony most people call me Andy. I Enjoy cooking, listening
+            to music, and playing with my dog. Looking forward to connecting to
+            other creatives.
+          </p>
+          <p>
+            <strong>Location:</strong> Vancouver, B.C.
+          </p>
         </div>
       </div>
       <div>
-        <h2 className="profile__Title">Recent Projects</h2>
+        <h3 className="profile__Title">Recent Projects</h3>
         <p className="profile_Description">Vegan Mac and Cheese</p>
         <p className="profile_Description">Landscape Mountains</p>
         <p className="profile_Description">Lyrical Poem</p>
       </div>
-      <button className="profile__Btn" onClick={() => toggleModal(false)}>CLOSE</button>
+      <div className="profile__BtnContainer">
+        <button className="profile__Btn" onClick={() => toggleModal(false)}>
+          Add Friend
+        </button>
+      </div>
     </div>
   );
 };
